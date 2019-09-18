67e0ff7e8587e746026dbe94730b1883070a038b
Diego Ramos <diego.coderamos@gmail.com>
2019-09-18 16:49:24 -0300

    update Feed.js layout

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
-import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import api from '../services/api';
+
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+} from 'react-native';
 
 import camera from '../assets/camera.png';
+import more from '../assets/more.png';
+import like from '../assets/like.png';
+import comment from '../assets/comment.png';
+import send from '../assets/send.png';
 
 class Feed extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -14,9 +27,62 @@ class Feed extends Component {
     ),
   });
 
+  state = {
+    feed: [],
+  };
+
+  async componentDidMount() {
+    // this.callSocket();
+    const response = await api.get('posts');
+    console.log('DATA:', response.data);
+    this.setState({ feed: response.data });
+  }
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.feed}
+          keyExtractor={post => post._id}
+          renderItem={({ item }) => (
+            <View style={styles.feedItem}>
+              <View style={styles.feedItemHeader}>
+                <View style={styles.userInfo}>
+                  <Text style={styles.name}>{item.author}</Text>
+                  <Text style={styles.place}>{item.place}</Text>
+                </View>
+                <Image source={more} />
+              </View>
+              <Image
+                style={styles.feedImage}
+                source={{ uri: `http://localhost:3333/${item.image}` }}
+              />
+
+              <View style={styles.feedItemFooter}>
+                <View style={styles.actions}>
+                  <TouchableOpacity onPress={() => {}}>
+                    <Image source={like} />
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => {}}>
+                    <Image source={comment} />
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => {}}>
+                    <Image source={send} />
+                  </TouchableOpacity>
+                </View>
+
+                <Text style={styles.likes}>{item.likes} curtidas</Text>
+                <Text style={styles.description}>{item.description}</Text>
+                <Text style={styles.hashtags}>{item.hashtags}</Text>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({});
 
 export default Feed;

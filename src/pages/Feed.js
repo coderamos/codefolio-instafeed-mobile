d1653b750a0ee1c8edb5f08b69febd7c6a61ec18
Diego Ramos <diego.coderamos@gmail.com>
2019-09-18 16:15:57 -0300

    config route and update header layout

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+import camera from '../assets/camera.png';
 
 class Feed extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerRight: (
+      <TouchableOpacity
+        style={{ marginRight: 20 }}
+        onPress={() => navigation.navigate('New')}>
+        <Image source={camera} />
+      </TouchableOpacity>
+    ),
+  });
+
   render() {
-    return (
-      <View>
-        <Text> feed/ </Text>
-      </View>
-    );
+    return <View />;
   }
 }
 
